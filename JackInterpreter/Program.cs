fb585c09a5772e0869dcd5b49577e9551d1eb6ab
Joel Johnston <colter13@gmail.com>
2024-02-02 17:19:06 -0700

    Scaffold some code

--- JackInterpreter/Program.cs
+++ JackInterpreter/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Antlr4.Runtime.Tree.Pattern;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var inputFile = @"C:\Users\colte\Downloads\project_11\Seven\Main.jack";
+
+        var inputStream = File.OpenRead(inputFile);
+
+        AntlrInputStream antlrInputStream = new AntlrInputStream(inputStream);
+        JackLexer lexer = new JackLexer(antlrInputStream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        JackParser parser = new JackParser(tokens);
+        IParseTree tree = parser.classDeclaration();
+
+        Console.WriteLine(tree.ToStringTree(parser));
+    }
+}
